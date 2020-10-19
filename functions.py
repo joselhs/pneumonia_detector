4891d92a148d70a7910efc5e0b3aa53f6cd77fdf
Jose L Holgado <jholgado@solverml.com>
2020-10-19 13:50:59 +0200

    predict, preprocess and load functions

--- functions.py
+++ functions.py
@@ -1,15 +1,35 @@
+import keras
+import numpy as np
+import streamlit as st
+from keras import layers, models, optimizers  # modeling
+from PIL import Image
+
+MODEL = "cnn_augmented_dropout_90_7.h5"
 
 
+@st.cache(allow_output_mutation=True)
 def load_model():
-    # TODO
     print("loading model")
+    model = keras.models.load_model(f"model/{MODEL}", compile=True)
+
+    return model
 
 
-def preprocess_image():
-    # TODO
+def preprocess_image(img):
     print("preprocessing image")
+    image = Image.open(img)
+    print(image)
+    p_img = image.resize((224, 224))
+
+    return np.array(p_img)
 
 
-def predict():
-    # TODO
+def predict(model, img):
     print("Predicting result")
+    print(f'shape: {img.shape}')
+
+    prediction = model.predict(np.reshape(img, [1, 224, 224, 3]))
+
+    print(prediction)
+    return prediction
+
